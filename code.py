7387981ada60f6b846959f0cfe8eb3d9c6297d5b
Joel Gustafson <joelg@mit.edu>
2016-10-23 06:47:14 -0400

    we've created a monster

--- code.py
+++ code.py
@@ -41,13 +41,9 @@ generator_output_shape = generator.blobs["deconv0"].data.shape
 
 mean = np.float32([104.0, 117.0, 123.0])
 
-net = caffe.Classifier("nets/placesCNN/places205CNN_deploy_updated.prototxt",
-                       "nets/placesCNN/places205CNN_iter_300000.caffemodel",
-                       mean = mean,            # ImageNet mean
-                       channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
 
-nsfw_net = caffe.Classifier("../open_nsfw/nsfw_model/deploy.prototxt",
-            		       "../open_nsfw/nsfw_model/resnet_50_1by2_nsfw.caffemodel",
+nsfw_net = caffe.Classifier("nets/open_nsfw/deploy.prototxt",
+            		       "nets/open_nsfw/resnet_50_1by2_nsfw.caffemodel",
                        mean = mean,            # ImageNet mean
                        channel_swap = (2,1,0)) # the reference model has channels in BGR order instead of RGB
 
@@ -102,96 +98,57 @@ def pad(classifier, image):
     o[:,:,topleft[0]:topleft[0]+image_size[0], topleft[1]:topleft[1]+image_size[1]] = image
     return o
 
-
+def get_code(path, layer):
+    batch_size = 1
+    image_size = (3, 227, 227)
+    images = np.zeros((batch_size,) + image_size, dtype='float32')
+    in_image = scipy.misc.imread(path)
+    in_image = scipy.misc.imresize(in_image, (image_size[1], image_size[2]))
+    for ni in range(images.shape[0]):
+        images[ni] = np.transpose(in_image, (2, 0, 1))
+    data = images[:,::-1]
+    matfile = scipy.io.loadmat('ilsvrc_2012_mean.mat')
+    image_mean = matfile['image_mean']
+    topleft = ((image_mean.shape[0] - image_size[1])/2, (image_mean.shape[1] - image_size[2])/2)
+    image_mean = image_mean[topleft[0]:topleft[0]+image_size[1], topleft[1]:topleft[1]+image_size[2]]
+    del matfile
+    data -= np.expand_dims(np.transpose(image_mean, (2,0,1)), 0) # mean is already BGR
+    encoder = caffe.Net(settings.encoder_definition, settings.encoder_weights, caffe.TEST)
+    encoder.forward(data=data)
+    feat = np.copy(encoder.blobs[layer].data)
+    del encoder
+    zero_feat = feat[0].copy()[np.newaxis]
+    return zero_feat, data
 
 np.random.seed(200)
 
-def get_code(path, layer):
-  '''
-  Push the given image through an encoder to get a code.
-  '''
-
-  # set up the inputs for the net:
-  batch_size = 1
-  image_size = (3, 227, 227)
-  images = np.zeros((batch_size,) + image_size, dtype='float32')
-
-  in_image = scipy.misc.imread(path)
-  in_image = scipy.misc.imresize(in_image, (image_size[1], image_size[2]))
-
-  for ni in range(images.shape[0]):
-    images[ni] = np.transpose(in_image, (2, 0, 1))
-
-  # Convert from RGB to BGR
-  data = images[:,::-1]
-
-  # subtract the ImageNet mean
-  matfile = scipy.io.loadmat('ilsvrc_2012_mean.mat')
-  image_mean = matfile['image_mean']
-  topleft = ((image_mean.shape[0] - image_size[1])/2, (image_mean.shape[1] - image_size[2])/2)
-  image_mean = image_mean[topleft[0]:topleft[0]+image_size[1], topleft[1]:topleft[1]+image_size[2]]
-  del matfile
-  data -= np.expand_dims(np.transpose(image_mean, (2,0,1)), 0) # mean is already BGR
-
-  # initialize the encoder
-  encoder = caffe.Net(settings.encoder_definition, settings.encoder_weights, caffe.TEST)
-
-  # run encoder and extract the features
-  encoder.forward(data=data)
-  feat = np.copy(encoder.blobs[layer].data)
-  del encoder
-
-  zero_feat = feat[0].copy()[np.newaxis]
-
-  return zero_feat, data
-
-init_file = "images/green_pepper.jpg"
+init_file = "test-jordan224.jpg"
 opt_layer = 'fc6'
 code, start_image = get_code(init_file, opt_layer)
-# code = np.random.normal(0, 1, shape)
-# code is numpy array of shape (1, 4096) = [[1, 0, ... ]]
 
-# total_iters h= 300
-total_iters = 100
+total_iters = 300
 
 alpha = 1
-# Load the activation range
 upper_bound = lower_bound = None
 
 # Set up clipping bounds
 upper_bound = np.loadtxt("act_range/3x/fc6.txt", delimiter=' ', usecols=np.arange(0, 4096), unpack=True)
 upper_bound = upper_bound.reshape(4096)
-# upper_bound is a numpy array of shape (4096,)
 
 # Lower bound of 0 due to ReLU
 lower_bound = np.zeros(4096)
-# lower_bound is also a numpy array of shape (4096,0)
-
-category = 0
 
 for i in range(0,total_iters):
     step_size = (alpha + (1e-10 - alpha) * i) / total_iters
-    # step_size = 0.5
-    gp, image = grad(net, 'fc8', category, code)
-    gp = gp.copy()
     gn, image = grad(nsfw_net, 'prob', 1, code)
 
-    # gp is a numpy array of shape (1, 4096)
-    # image is a nupmy array of shape (1, 3, 227, 227)
-
-    # To generate NSFW Samples
-    # g =  1500*gn + 0.00000000001 * gp
     g = 1500*gn
 
-    # To generate Regular Samples
-    # g = 1*gp + 0*gn
+    # print norm(gn)
 
-    # print norm(gp), norm(gn)
-    print norm(g)
-
-    # if norm(g) <= 1e-8:
-    #     break
-    code = code - step_size * g / np.abs(g).mean()
+    if norm(g) <= 1e-8:
+        break
+    code = code - step_size*g/np.abs(g).mean()
     code = np.maximum(code, lower_bound)
 
     # 1*upper bound produces realistic looking images
@@ -199,6 +156,4 @@ for i in range(0,total_iters):
     # 1.5* Upper bound is a decent choice
     code = np.minimum(code, 1.5*upper_bound)
 
-    if i % 10 == 0 or True:
-        print "saving image"
-        save_image(image, "final_places/result-%d.jpg" % i)
+    save_image(image, "output/" + str(i) + ".jpg")
